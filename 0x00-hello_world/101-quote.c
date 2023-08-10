c83eabb097cace9931f4eff971c1741b04ad8a3d
SSENTAMU TONNY <ssentamutonny03@gmail.com>
2023-08-10 10:06:39 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main() {
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, sizeof(message) - 1);
+    return 1;
+}
