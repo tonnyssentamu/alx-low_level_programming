88c30749d804754ca6968caa566dcb4946412a5c
SSENTAMU TONNY <ssentamutonny03@gmail.com>
2023-08-10 10:19:25 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,10 @@
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int main() {
-    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, sizeof(message) - 1);
-    return 1;
+int main(void)
+{
+	char message[] = "and that piece of art is useful
+		\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, sizeof(message) - 1);
+	return (1);
 }
