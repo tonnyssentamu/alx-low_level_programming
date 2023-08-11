0ae85d0ad7917fe676c968c0233a8426d7dadc5b
SSENTAMU TONNY <ssentamutonny03@gmail.com>
2023-08-11 02:09:47 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include<time.h>
+/*
+ * my main will contian a coditional statement
+ * below is the betty's style for the main function
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is postive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative \n', n);
+	}
+
+	return (0);
+}
