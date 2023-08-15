6de389794f53adb636d56d2a2229fb67fb7731ce
SSENTAMU TONNY <ssentamutonny03@gmail.com>
2023-08-15 10:09:20 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef "main_h"
+#define "main_h"
 #include <unistd.h>
 
 int _putchar(char c);
@@ -11,3 +13,6 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_to_98(int n);
+
+#endif
