4964a69a88882e30f8cceb4ad1a006d769189ba1
SSENTAMU TONNY <ssentamutonny03@gmail.com>
2023-08-15 08:26:26 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
