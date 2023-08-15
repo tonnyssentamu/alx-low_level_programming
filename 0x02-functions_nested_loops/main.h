015f8f055801fc918084f2907d4390ddb4516837
SSENTAMU TONNY <ssentamutonny03@gmail.com>
2023-08-15 10:13:15 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,4 @@
-#ifndef "main_h"
-#define "main_h"
+
 #include <unistd.h>
 
 int _putchar(char c);
@@ -14,5 +13,3 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-
-#endif
