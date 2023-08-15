7333e7f455a8c24df16094e8daf43a304e17cd94
SSENTAMU TONNY <ssentamutonny03@gmail.com>
2023-08-15 08:44:46 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char d;
+	int i = 0;
+
+	while (i < 9)
+	{
+		for (d = 'a'; d <= 'z'; d++)
+		{
+			_putchar(d);
+		}
+
+		i++;
+
+		_putchar('\n');
+	}
+}
