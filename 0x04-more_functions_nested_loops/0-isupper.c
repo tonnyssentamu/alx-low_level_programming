aa2b804154e5993f1c9259fec5fc704ed8df9650
SSENTAMU TONNY <ssentamutonny03@gmail.com>
2023-08-17 02:50:34 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c: a character to check
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	char c;
+
+	if (c >= 'A' && C <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
