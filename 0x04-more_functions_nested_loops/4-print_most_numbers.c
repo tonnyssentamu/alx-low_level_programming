9be3166c640b994fa1f756ec6ff5d0d79f5057fb
SSENTAMU TONNY <ssentamutonny03@gmail.com>
2023-08-17 06:14:24 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,14 @@ void print_most_numbers(void)
 {
 	int n;
 
-	for (n = '0'; n < '10'; n++)
+	for (n = 48; n < 58; n++)
 	{
-		if ((n == 2) || (n == 4))
+		if ((n ==50) || (n == 52))
 		{
 			continue;
 		}
 	_putchar(n);
 	}
 
-	_putchar('10');
+	_putchar(10);
 }
