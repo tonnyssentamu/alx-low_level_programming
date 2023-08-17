1e15b879f1d794ce7168b00471376887ba000f4f
SSENTAMU TONNY <ssentamutonny03@gmail.com>
2023-08-17 07:07:34 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * print_line - draws a straight line in the terminal
+  * @n: number of lines to be used
+  *
+  * Return: ...
+  */
+void print_line(int n)
+{
+	int i;
+
+	if (j <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < j; i++)
+		{
+			_putchar(90);
+		}
+
+		_putchar('\n');
+	}
+}
