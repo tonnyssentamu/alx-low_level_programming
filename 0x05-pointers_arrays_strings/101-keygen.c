6f0c6cff87035c12aced780528837d0617f7978f
SSENTAMU TONNY <ssentamutonny03@gmail.com>
2023-08-21 20:21:51 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,14 +8,14 @@
  */
 int main(void)
 {
-	char password[80];
+	char password[84];
 	int index = 0, sum = 0, diff_half1, diff_half2;
 	
 	srand(time(0));
 
 	while (sum < 2772)
 	{
-		password[index] = 33 +rand() %94;
+		password[index] = 33 +rand() % 94;
 		sum += password[index++];
 	}
 	password[index] = '\0';
