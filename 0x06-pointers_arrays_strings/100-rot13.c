458165a143fcec59b002bb73d81810bc45e4eda2
SSENTAMU TONNY <ssentamutonny03@gmail.com>
2023-08-24 14:31:30 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rot13 - lfunction that encodes a string using rot13.
+ * rot13 - function that encodes a string using rot13.
  * @s: string
  * Return: string `s` rotated
  */
@@ -9,15 +9,15 @@
 char *rot13(char *s)
 {
 	int i;
-	char storeh[] = "ABCDEFGHIJKLM";
-	char storel[] = "abcdefghijklm";
+	char x[] = "ABCDEFGHIJKLM";
+	char y[] = "abcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
 		{
 			s[i] = (s[i] - 65 > 25) ?
-				storel[s[i] - 97] : storeh[s[i] - 65];
+				x[s[i] - 97] : y[i] - 65];
 		}
 	}
 	return (s);
